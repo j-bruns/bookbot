9914ce0dc1082d3815a4ba08ac33b3f84d871089
Jonas-M-Bruns <github@jonas.ruhr>
2025-10-06 12:47:26 +0200

    fixed bug

--- stats.py
+++ stats.py
@@ -6,6 +6,7 @@ def number_of_chars(text):
         char = char.lower()
         if not {"char":char,"num":0} in result:
             result.append({"char":char,"num":0})
+    for char in text:
         for i in range(0,len(result)):
             if result[i]["char"] == char:
                 result[i]["num"] += 1
