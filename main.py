d8dc3c13bbcfe60febfb2c0c3b5c000dac1921ce
Jonas-M-Bruns <github@jonas.ruhr>
2025-10-06 09:44:58 +0200

    added file reader

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file, "r") as file:
+        file_content = file.read()
+        return file_content
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+main()
